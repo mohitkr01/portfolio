2f3017dc3792912d4c879a817e8a87c244040e40
Mohit Kumar <mohit.kumar@curtisfitch.com>
2021-01-15 20:11:28 +0530

    resume update

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -36,7 +36,7 @@ const Profile = () => {
                 <Skills />
             </div>
             <div className="profile__contact">
-                <a href="https://drive.google.com/file/d/1xqodRv_cgxmi6khnNQLOnPkxXYvzZkUK/view?usp=sharing" target="_blank" rel="noreferrer">
+                <a href="https://drive.google.com/file/d/1ilLJgrnG1BYhVV2a_8pX4JxMidxftCD3/view?usp=sharing" target="_blank" rel="noreferrer">
                     <span>
                         Download CV
                         </span>
